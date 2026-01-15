bc1f25c45c60c241bfb607bb4361201e00ffff82
XI-2870 <harpreet.singh1@xebia.com>
2026-01-15 18:14:58 +0530

    v1.1.3: Fix var. prefix in main.tf references

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "this" {
   ami           = var.aws_instance_ami
-  instance_type = aws_instance_size
+  instance_type = var.aws_instance_size
   key_name      = var.aws_instance_key
 
   tags = {
